9ae06e4548be99e2e4dcf93cba2ce702684fcd50
alinegfchagas <alinegfchagas@hotmail.com>
2022-08-03 14:52:06 -0300

    atualização chakra

--- src/Routes/Coordinator.js
+++ src/Routes/Coordinator.js
@@ -10,39 +10,35 @@ export const goToHomePage = (navigate) => {
   navigate("/");
 };
 
-<<<<<<< HEAD
-export const goToRestaurantDetailPage = (navigate, id) => {
-  navigate(`restaurantes/detalhes/${id}`);
-};
-export const goToRestaurantPage = (navigate,id) => {
-  navigate(`restaurantes/${id}`);
-};
+// export const goToRestaurantDetailPage = (navigate, id) => {
+//   navigate(`restaurantes/detalhes/${id}`);
+// };
+// export const goToRestaurantPage = (navigate,id) => {
+//   navigate(`restaurantes/${id}`);
+// };
+
+// export const goToCartPage = (navigate) => {
+//   navigate("/carrinho");
+// };
+// export const goToProfilePage = (navigate) => {
+//   navigate("/perfil");
+// };
+
+// export const goToLastPage = (navigate) => {
+//   navigate(-1);
+// };
+
+//   export const goToRestaurantDetailPage = (navigate, id) => {
+//     navigate(`/detalhes/${id}`);
+//   };
 
 export const goToCartPage = (navigate) => {
   navigate("/carrinho");
 };
 export const goToProfilePage = (navigate) => {
-  navigate("/perfil");
+  navigate("/myProfile");
 };
 
 export const goToLastPage = (navigate) => {
   navigate(-1);
 };
-=======
-  
-//   export const goToRestaurantDetailPage = (navigate, id) => {
-//     navigate(`/detalhes/${id}`);
-//   };
-  
-  export const goToCartPage = (navigate) => {
-    navigate("/carrinho");
-  };
-  export const goToProfilePage = (navigate) => {
-    navigate("/myProfile");
-  };
-  
-  export const goToLastPage = (navigate) => {
-    navigate(-1);
-  };
-  
->>>>>>> b20535d5a7ae25af9a96053304a801415df60670
