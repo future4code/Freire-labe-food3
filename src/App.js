be57b661f683f31567b82e66f1fd2e06cb03cabb
joaoVictorVictoy <joaovvag@gmail.com>
2022-08-07 12:03:14 -0300

    Inicialização mobile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Router from "./Routes/Router";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
 import GlobalState from "./Global/GlobalState";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider >
+      <Flex m={'5'} h='800px' w="500px" border='10px solid black'  borderRadius={'25'} overflowY={"700px"} flexDirection={'column'}>
       <GlobalState> 
+      
       <Router>
       </Router>
+      
       </GlobalState>
+      </Flex>
     </ChakraProvider>);
 }
 export default App;
